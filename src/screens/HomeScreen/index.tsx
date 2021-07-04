ed05ba338c74f5bbe9ba858a88503d5e573496c7
atulrajdhar <atulrajdhar@users.noreply.github.com>
2021-07-04 17:32:45 +0530

    HomeScreen - SearchOption

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
+import {View, StyleSheet, FlatList} from 'react-native';
 import ProductItem from '../../components/ProductItem';
 
 import products from '../../data/products';
 
-const HomeScreen = () => {
+const HomeScreen = ({searchValue}: {searchValue: string}) => {
+  console.log(searchValue);
   return (
     <View style={styles.page}>
       {/* Render Product Component */}
       <FlatList
         data={products}
         renderItem={({item}) => <ProductItem item={item} />}
-        keyExtractor={({ id }) => id}
+        keyExtractor={({id}) => id}
         showsVerticalScrollIndicator={false}
-      />      
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    page: {        
-        padding: 10,
-    }
+  page: {
+    padding: 10,
+  },
 });
 
 export default HomeScreen;
